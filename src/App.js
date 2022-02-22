a4c9ae70477e56ed78491b26236667c618ae04a7
Ricardo Ronchetti <ricardoronchetti@gmail.com>
2022-02-22 16:21:29 +0100

    updated src files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import contactsArray from "./contacts.json"
+import hasWon from './components/hasWon'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [contacts, setContacts] = useState(contactsArray.splice(0, 5))
+
+  const changeContact = event => setContacts(event.target.value)
+
+  return <table className="App">
+    <p><strong>IronContacts</strong></p>
+    <article>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map(elm => {
+            return (
+              <tr key={elm._id}>
+                <td><img src={elm.pictureUrl} alt={elm.name} /></td>
+                <td>{elm.name}</td>
+                <td>{elm.popularity}</td>
+                {/* 
+                <td>if (elm.wonOscar) {
+                    return <td>trophy</td>
+                  } else if (elm.wonEmmy){
+                    return <td>star</td>
+                  } else {
+                  }</td> */}
+
+                <td>hasWon props={elm.wonOscar}</td>
+                <td>hasWon props={elm.wonEmmy}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </article>
+  </table>
 }
 
-export default App;
+export default App
---
+++ src/components/hasWon.js
@@ -0,0 +1,10 @@
+const hasWon = ({ props }) => {
+    if (props === true) {
+        return props = '🏆'
+    } else {
+        return
+    }
+
+}
+
+export default hasWon
